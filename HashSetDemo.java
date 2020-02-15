22cde8f92378658e46f3b9e160b2c335d6b51545
Uttam Yadav <uttam.yadav8299@gmail.com>
2020-02-16 00:05:16 +0530

    Java Collections - HashSet Simple Program

---
+++ HashSetDemo.java
@@ -0,0 +1,18 @@
+// Demonstrating hashSet 
+
+import java.util.*;
+
+public class HashSetDemo{
+ 	public static void main(String args[]){
+	 HashSet<String> hs = new HashSet<String>();
+	 
+ 	 hs.add("B" );
+	 hs.add("A");
+	 hs.add("D");
+	 hs.add("E");
+  	 hs.add("C");
+	 hs.add("F");
+	 
+	 System.out.println(hs);
+ }
+}
